fd98475b0a596318f6a008f0be4cfeee675300ea
Ugniusst <ugnius.staniukynas@mif.stud.vu.lt>
2023-01-19 01:04:20 +0200

    split functions

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 setTimeout(() => {
     findInconsistenButtons(document.body);
-  }, 3000)
+  }, 1000)
 
 
 
@@ -8,21 +8,28 @@ function findInconsistenButtons(body) {
     var divs = body.getElementsByTagName("div");
     for (let div of divs) {
         if(div.getElementsByTagName("div").length == 0) {
-            var buttons = div.getElementsByTagName("button");
-            if(buttons.length > 1) {
-                lastIndex = buttons.length - 1;
-                latestClass = buttons[lastIndex].className;
-                if(buttons[lastIndex].hasAttribute("mode")) {
-                    latestMode = buttons[lastIndex].getAttribute("mode");
-                }
-                console.log(latestClass);
-                for (let button of buttons) {
-                    button.className = latestClass;
-                    if(button.hasAttribute("mode")) {
-                        button.setAttribute("mode", latestMode);
-                    }
-                }
-            }
+           found = modifyButtonStyles(div);
         }
     }
+}
+
+function modifyButtonStyles(div) {
+    var buttons = div.getElementsByTagName("button");
+    if(buttons.length > 1) {
+        lastIndex = buttons.length - 1;
+        latestClass = buttons[lastIndex].className;
+        if(buttons[lastIndex].hasAttribute("mode")) {
+            latestMode = buttons[lastIndex].getAttribute("mode");
+        }
+        console.log(latestClass);
+        for (let button of buttons) {
+            button.className = latestClass;
+            if(button.hasAttribute("mode")) {
+                button.setAttribute("mode", latestMode);
+            }
+        }
+        return true;
+    }
+    else return false;
+
 }
