943de8eda78998a776308dc996cadbdc409b5278
Ugniusst <ugnius.staniukynas@mif.stud.vu.lt>
2023-01-19 01:21:21 +0200

    Added comments

--- scripts/script.js
+++ scripts/script.js
@@ -6,7 +6,9 @@ setTimeout(() => {
 
 function findInconsistenButtons(body) {
     var divs = body.getElementsByTagName("div");
+    //check all divs
     for (let div of divs) {
+        //if div doesnt have child "div" nodes, then search for buttons
         if(div.getElementsByTagName("div").length == 0) {
            found = modifyButtonStyles(div);
         }
@@ -15,6 +17,7 @@ function findInconsistenButtons(body) {
 
 function modifyButtonStyles(div) {
     var buttons = div.getElementsByTagName("button");
+    //if one or more buttons exist there, change their style to last button's class.
     if(buttons.length > 1) {
         lastIndex = buttons.length - 1;
         latestClass = buttons[lastIndex].className;
